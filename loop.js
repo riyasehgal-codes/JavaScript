1085c523e12bf99c488c1a1a7cfea5fc11213199
riyasehgal-codes <riyasehgal00@gmail.com>
2024-11-21 00:14:02 +0530

    basic javascript

---
+++ loop.js
@@ -0,0 +1,10 @@
+// for loop
+
+// for ( let i=0; i<=5;i++){
+//     console.log("jello");
+// }
+let i=0;
+while (i<=5){
+    console.log("jello");
+    i++;
+}
---
+++ vote.js
@@ -0,0 +1,6 @@
+let age= prompt("Enter your age");
+if (age>=18){
+    console.log("You can vote");
+}else {
+    console.log("You cannot vote");
+}
